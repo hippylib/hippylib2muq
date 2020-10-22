e989b80e2e05af5d3494e5bbb9108d1dbb426839
Umberto Villa <uvilla@wustl.edu>
2020-10-22 17:10:17 -0500

    Update __init__.py
    Include version

--- hippylib2muq/__init__.py
+++ hippylib2muq/__init__.py
@@ -1,3 +1,24 @@
+#  hIPPYlib-MUQ interface for large-scale Bayesian inverse problems
+#  Copyright (c) 2019-2020, The University of Texas at Austin, 
+#  University of California--Merced, Washington University in St. Louis,
+#  The United States Army Corps of Engineers, Massachusetts Institute of Technology
+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# version
+from .version import version_info, __version__
+
 from .utility import *
 from .interface import *
 from .mcmc import *
